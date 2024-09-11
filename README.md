caf627f90a68e120318a71180987b8c5b93664a6
SimonHansul <47112277+SimonHansul@users.noreply.github.com>
2024-09-11 23:14:11 +0200

    proper package setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# mempyDEB
+
+mempyDEB is a Python package that provides the basics for doing DEB-TKTD modelling in Python. 
+It mostly defines a baseline model, default parameters and functions to run the model.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mempyDEB',
+    version='0.1.0',
+    description='DEB-TKTD modelling in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Simon Hansul',
+    author_email='simonhansul@gmail.com',
+    url='https://github.com/simonhansul/mempyDEB',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy'
+        ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
