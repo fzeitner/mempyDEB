425961374217c2555f65fb038d9020e7054f4614
fzeitner <fzeitner@uni-osnabrueck.de>
2025-03-04 14:00:03 +0100

    first control fit test

--- folsomia_project/DEBfitting_Folsomia.py
+++ folsomia_project/DEBfitting_Folsomia.py
@@ -43,7 +43,7 @@ def load_data():
     Einlesen der Kontrolldaten für Folsomia.
     """
 
-    data = pd.read_csv('../../data/folsomia_cadmium/folsomia_temperature_cadmium_growth_tidy.csv', header = 5)
+    data = pd.read_csv('folsomia_temperature_cadmium_growth_tidy.csv', header = 5)
     data = data.loc[lambda df : df.T_cels == 20]
     data = data.loc[lambda df : df.C_F == 0]
     data = data.assign(S = collembola_length_to_weight(data.length_mm))
--- folsomia_project/TKTDFitting_Folsomia.py
+++ folsomia_project/TKTDFitting_Folsomia.py
@@ -43,7 +43,7 @@ def load_data():
     Einlesen der Kontrolldaten für D. magna.
     """
     
-    data = pd.read_csv('../../data/folsomia_cadmium/folsomia_temperature_cadmium_growth_tidy.csv', header = 5)
+    data = pd.read_csv('folsomia_temperature_cadmium_growth_tidy.csv', header = 5)
     data = data.loc[lambda df : df.T_cels == 20]
     data = data.assign(S = collembola_length_to_weight(data.length_mm))
     data.rename(columns = {'C_F' : 'C_W'}, inplace = True)
